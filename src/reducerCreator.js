39a86571d9aabc6171de2ac49c6806d257938c8d
rodrigocipriani <chopsss@gmail.com>
2017-09-10 18:21:14 -0300

    no more reducer creator

--- src/reducerCreator.js
+++
@@ -1,9 +0,0 @@
-
-const reducerCreator = (initialState, actionsMap) => {
-  return (state = initialState, action = {}) => {
-    const fn = actionsMap[action.type];
-    return fn ? fn(state, action) : state;
-  };
-};
-
-export default reducerCreator;
